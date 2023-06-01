e47554c367cc0fc431b2a1a4c03d6fcc3cded791
BenoitSecurity <135039459+BenoitSecurity@users.noreply.github.com>
2023-06-01 16:48:27 +0200

    Update getscripts.sh

--- getscripts.sh
+++ getscripts.sh
@@ -2,17 +2,17 @@
 
 # Download ONX Benoit
 
-wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/onxbenoit.sh?token=GHSAT0AAAAAACDJF5IFQD5ZCPSVNULPRDYWZDYVVRA && sudo chmod +x ./onxbenoit.sh
+wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/onxbenoit.sh && sudo chmod +x ./onxbenoit.sh
 
 # Download conf scripts zabbix
 
-wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_agentd.conf?token=GHSAT0AAAAAACDJF5IF3QARVWHSO7MSQRHCZDYVWTA
-wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_proxy.conf?token=GHSAT0AAAAAACDJF5IESQSFZGW6QV2J4I56ZDYVXBQ
+wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_agentd.conf
+wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_proxy.conf
 
 # Download Zabbix installatie file
 
-wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_install_arm.sh?token=GHSAT0AAAAAACDJF5IFPLWWQXPBKHV5TEAYZDYVX2Q
+wget https://raw.githubusercontent.com/BenoitSecurity/scripts/main/zabbix_install_arm.sh && sudo chmod +x ./zabbix_install_arm.sh
 
 # Download Watool
 
-wget https://raw.githubusercontent.com/optimanetworks/onx/main/config/watool.sh && sudo chmod +x ./watool.sh && sudo ./watool.sh
+wget https://raw.githubusercontent.com/optimanetworks/onx/main/config/watool.sh && sudo chmod +x ./watool.sh
